c66414a0b52620a3a272471e061ce8bf1300cf26
Viktor Borszeki <vborszeki@gmail.com>
2019-07-12 12:36:58 +0200

    Updates

---
+++ precache-manifest.713a21c73da93616019e4a31b9398615.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c420696ae26e5616501303966a11ecc",
+    "url": "/pedalem/index.html"
+  },
+  {
+    "revision": "6a9ca810c5ecbe127c00",
+    "url": "/pedalem/static/js/2.16728bb1.chunk.js"
+  },
+  {
+    "revision": "6048d0d0074a666256b3",
+    "url": "/pedalem/static/js/main.c32f46d8.chunk.js"
+  },
+  {
+    "revision": "8091c1516ce9aec53d1e",
+    "url": "/pedalem/static/js/runtime~main.239b2c4c.js"
+  }
+]);
